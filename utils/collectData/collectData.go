e105abc704a67420a4c6798c9259f77bdd83ed1b
ldrv565 <ldrv5555@gmail.com>
2018-10-25 01:27:50 +0300

    Рефакторинг

---
+++ utils/collectData/collectData.go
@@ -0,0 +1,103 @@
+package collectData
+
+import (
+	"archive/zip"
+	"bytes"
+	"encoding/json"
+	"io"
+	"io/ioutil"
+	"os"
+	"path"
+)
+
+//FileMeta - единица передачи метаданных файла
+type FileMeta struct {
+	Name string // `json: "filename"`
+}
+
+//FileCollector - для сбора итогового файла
+type FileCollector struct {
+	ZipBuf   *bytes.Buffer
+	Zip      *zip.Writer
+	MetaData []*FileMeta
+}
+
+//NewFileCollector - конструктор FileCollector
+func NewFileCollector() *FileCollector {
+	buf := new(bytes.Buffer)
+	return &FileCollector{
+		ZipBuf:   buf,
+		Zip:      zip.NewWriter(buf),
+		MetaData: make([]*FileMeta, 0, 100),
+	}
+}
+
+//walkFiles - обход всех файлов внутри указанной директории
+func (f *FileCollector) WalkFiles(filepath string) (err error) {
+	var files []os.FileInfo
+	var fileReader *os.File
+
+	if files, err = ioutil.ReadDir(filepath); err != nil {
+		return
+	}
+
+	for i := range files {
+
+		fullPath := path.Join(filepath, files[i].Name())
+
+		if files[i].IsDir() {
+			if err = f.WalkFiles(fullPath); err != nil {
+				return
+			}
+			continue
+		}
+		f.addMeta(fullPath)
+
+		if fileReader, err = os.Open(fullPath); err != nil {
+			return
+		}
+
+		if err = f.packFile(fullPath, fileReader); err != nil {
+			return
+		}
+	}
+
+	return
+}
+
+func (f *FileCollector) packFile(filename string, fileReader io.Reader) (err error) {
+	var fileWriter io.Writer
+
+	if fileWriter, err = f.Zip.Create(filename); err != nil {
+		return
+	}
+
+	if _, err = io.Copy(fileWriter, fileReader); err != nil {
+		return
+	}
+
+	return
+}
+
+func (f *FileCollector) Meta2json() (js []byte, err error) {
+	return json.Marshal(f.MetaData)
+}
+
+func (f *FileCollector) addMeta(fullPath string) {
+
+	f.MetaData = append(f.MetaData, &FileMeta{
+		Name: fullPath,
+	})
+
+	return
+}
+
+func (f *FileCollector) ZipData() (data []byte, err error) {
+
+	if err = f.Zip.Close(); err != nil {
+		return
+	}
+
+	data = f.ZipBuf.Bytes()
+	return
+}
--- utils/zipData/zipData.go
+++ utils/zipData/zipData.go
@@ -1,14 +1,9 @@
 package zipData
 
 import (
-	"archive/zip"
-	"bytes"
-	"encoding/json"
 	"fmt"
-	"io"
 	"io/ioutil"
-	"os"
-	"path"
+	"lab1/utils/collectData"
 )
 
 //ZipFiles - создаем сборщик метаданных, передаем его методу перебора указанной директории,
@@ -16,15 +11,15 @@ import (
 //сохраняем архив
 func ZipFiles() (err error) {
 
-	collector := NewFileCollector()
+	collector := collectData.NewFileCollector()
 
-	if err = walkFiles(collector, "./filesDir"); err != nil {
+	if err = collector.WalkFiles("./filesDir"); err != nil {
 		return
 	}
 
 	var js []byte
 
-	if js, err = collector.meta2json(); err != nil {
+	if js, err = collector.Meta2json(); err != nil {
 		return
 	}
 
@@ -32,7 +27,7 @@ func ZipFiles() (err error) {
 
 	var zipData []byte
 
-	if zipData, err = collector.zipData(); err != nil {
+	if zipData, err = collector.ZipData(); err != nil {
 		return
 	}
 
@@ -40,97 +35,5 @@ func ZipFiles() (err error) {
 		return
 	}
 
-	return
-}
-
-func walkFiles(collector *FileCollector, filepath string) (err error) {
-	var files []os.FileInfo
-
-	if files, err = ioutil.ReadDir(filepath); err != nil {
-		return
-	}
-
-	for i := range files {
-
-		fullPath := path.Join(filepath, files[i].Name())
-
-		if files[i].IsDir() {
-			if err = walkFiles(collector, fullPath); err != nil {
-				return
-			}
-			continue
-		}
-		collector.addMeta(fullPath)
-
-		var fileReader *os.File
-
-		if fileReader, err = os.Open(fullPath); err != nil {
-			return
-		}
-
-		if err = collector.packFile(fullPath, fileReader); err != nil {
-			return
-		}
-	}
-
-	return
-}
-
-//FileMeta - единица передачи метаданных файла
-type FileMeta struct {
-	Name string // `json: "filename"`
-}
-
-//FileCollector - для сбора итогового файла
-type FileCollector struct {
-	ZipBuf   *bytes.Buffer
-	Zip      *zip.Writer
-	MetaData []*FileMeta
-}
-
-//NewFileCollector - конструктор FileCollector
-func NewFileCollector() *FileCollector {
-	buf := new(bytes.Buffer)
-	return &FileCollector{
-		ZipBuf:   buf,
-		Zip:      zip.NewWriter(buf),
-		MetaData: make([]*FileMeta, 0, 100),
-	}
-}
-
-func (f *FileCollector) meta2json() (js []byte, err error) {
-	return json.Marshal(f.MetaData)
-}
-
-func (f *FileCollector) addMeta(fullPath string) {
-
-	f.MetaData = append(f.MetaData, &FileMeta{
-		Name: fullPath,
-	})
-
-	return
-}
-
-func (f *FileCollector) packFile(filename string, fileReader io.Reader) (err error) {
-	var fileWriter io.Writer
-
-	if fileWriter, err = f.Zip.Create(filename); err != nil {
-		return
-	}
-
-	if _, err = io.Copy(fileWriter, fileReader); err != nil {
-		return
-	}
-
-	return
-}
-
-func (f *FileCollector) zipData() (data []byte, err error) {
-
-	if err = f.Zip.Close(); err != nil {
-		return
-	}
-
-	data = f.ZipBuf.Bytes()
 	return
 }
