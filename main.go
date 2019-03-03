2f5e4170dcf6778c38184b57021bb7e4c1a7eb16
ldrv565 <40745264+ldrv565@users.noreply.github.com>
2019-03-03 16:04:36 +0300

    Заготовка для сервера

--- main.go
+++ main.go
@@ -1,5 +1,40 @@
 package main
 
+import (
+	"fmt"           // пакет для форматированного ввода вывода
+	"html/template" // пакет для логирования
+	"net/http"      // пакет для поддержки HTTP протокола
+	// пакет для работы с  UTF-8 строками
+)
+
+var text string
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("./index.html")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+
+	fmt.Println(text)
+
+	t.ExecuteTemplate(w, "index", text)
+
+}
+
+func drawHandler(w http.ResponseWriter, r *http.Request) {
+	content := r.FormValue("content")
+
+	text = text + content
+
+	http.Redirect(w, r, "/", 302)
+}
+
 func main() {
 
+	text = ""
+
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/draw", drawHandler)
+
+	http.ListenAndServe(":3000", nil)
 }
